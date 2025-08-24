614ca59e45d4a3c89c191fd58cf2149da16c51aa
kaio <kaio22barra@gmail.com>
2025-08-24 19:00:06 -0300

    Update ContaBancaria.java

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ContaBancaria {
+    String numeroConta;
+    String titular;
+    double saldo;
+
+
+    public void consultarSaldo() {
+        System.out.println("Conta: " + numeroConta + " | Titular: " + titular + " | Saldo atual: R$" + saldo);
+    }
+
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito de R$" + valor + " realizado com sucesso!");
+        } else {
+            System.out.println("Valor inválido para depósito.");
+        }
+    }
+
+
+    public void sacar(double valor) {
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
+            System.out.println("Saque de R$" + valor + " realizado com sucesso!");
+        } else if (valor > saldo) {
+            System.out.println("Saldo insuficiente para saque!");
+        } else {
+            System.out.println("Valor inválido para saque.");
+        }
+    }
 }
